9c591d1299e60d1ecf4d7dc6142aa9cae9134c1d
Richard D. Walsh <richard@thechirp.ca>
2016-05-17 21:31:40 -0600

    Add Geolocation to Map

--- assets/js/map.js
+++ assets/js/map.js
@@ -41,4 +41,60 @@ $(document).ready(function() {
     }
 
   });
+
+  if (navigator.geolocation) {
+    getCurrentLocation();
+  } else {
+    alert("Geolocation API not available in this browser");
+  }
 });
+
+function getCurrentLocation() {
+  // Use HTML5 Geolocation API to get Current Position
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback, { timeout: 10000 });
+}
+
+function successCallback(result) {
+  var lat = result.coords.latitude;
+  var lng = result.coords.longitude;
+  var myLatLng = new google.maps.LatLng(lat, lng);
+  var mapOptions = {
+    center: myLatLng,
+    zoom: 10,
+    draggable: false,
+    zoomControl: false,
+    scaleControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    mapTypeControl: false
+  };
+
+  var map = new google.maps.Map($("#map").get(0), mapOptions);
+
+  // Add Marker
+  new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: "My Current Location"
+  })
+}
+
+function errorCallback(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      alert("User location permission denied.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert("User location unavailable.");
+      break;
+    case error.PERMISSION_DENIED_TIMEOUT:
+      alert("User took too long to grant/deny permission.");
+      break;
+    case error.UNKNOWN_ERROR:
+      alert("An Unknown Error Occurred.");
+      break;
+    default:
+      alert("An Unknown Error Occurred.");
+      break;
+  }
+}
